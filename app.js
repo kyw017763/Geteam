ffba6c146b2ffa7b253aef80447532b0354cdd82
kyw017763 <kyw017763@gmail.com>
2019-06-22 23:56:18 +0900

    Party Directory, Set module

---
+++ app.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const path = require('path');
+// const sequelize = require('sequelize'); // ORM 사용시
+const parseJson = require('parse-json');
+const fs = require('fs');
+// const spawn = require('child_process').spawn; // 자식모듈
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+// const fb = require(./function/fbLogin'); // facebook login
+const passport = require('passport')
+// const language = require('@google-cloud/language');
+//templete engine and path
+app.set('view engine','pug');
+app.set('views','./views');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cookieParser())
+app.use(session({
+	secret:'123123123',
+	resave:false,
+	saveUninitialize:true
+}));
+
+//sha 256 비밀번호 암호화
+var sha256 = require('sha256');
+
+//templete engine and path
+app.set('views', path.join(__dirname, '/views'));
+app.set("view engine", 'html');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cookieParser())
+app.use(session({
+	secret:'123123123',
+	resave:false,
+	saveUninitialize:true
+}));
+
+const route = require('./route.js');
+
+app.use((req, res, next) => {
+  console.log('Use Module');
+  next();
+});
+
+app.use(express.static(__dirname));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// 위에서 path.join을 해주었으므로 .use 만 사용하면 됨
+app.use(route);
+
+app.use((req, res, next) => { // 404 처리 부분
+  res.status(404).send('일치하는 주소가 없습니다!');
+});
+app.use((err, req, res, next) => { // 에러 처리 부분
+  console.error(err.stack); // 에러 메시지 표시
+  res.status(500).send('서버 에러!'); // 500 상태 표시 후 에러 메시지 전송
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('zteam on port 3000!');
+}); // 이전과 동일
--- route.js
+++ route.js
@@ -4,52 +4,53 @@ const router = express.Router(); // 라우터 분리
 
 router.get('/', (req, res) => {
     console.log('index page');
-    res.sendFile(path.join(__dirname, 'index.php'));
+    res.render(path.join(__dirname, '.views/index.php'));
 });
 
 router.get('/signup', (req, res) => {
     console.log('signup page');
-    res.sendFile(path.join(__dirname, 'signup.php'));
+    res.sendFile(path.join(__dirname, '.views/signup.php'));
 });
 
 router.get('/signin', (req, res) => {
     console.log('signin page');
-    res.sendFile(path.join(__dirname, 'signin.php'));
+    res.sendFile(path.join(__dirname, '.views/signin.php'));
 });
 
 router.get('/study/list', (req, res) => {
     console.log('study_list page');
-    res.sendFile(path.join(__dirname, 'study_list.php?kind=',req.query.kind,'&big=',req.query.big));
+    res.sendFile(path.join(__dirname, '.views/study_list.php?kind=',req.query.kind,'&big=',req.query.big));
 });
 
 router.get('/study/view', (req, res) => {
     console.log('study_view page');
-    res.sendFile(path.join(__dirname, 'study_view.php?num=',req.query.num,'&page=',page,'&kind=',req.query.kind,'&big=',req.query.big));
+    res.sendFile(path.join(__dirname, '.views/study_view.php?num=',req.query.num,'&page=',page,'&kind=',req.query.kind,'&big=',req.query.big));
 });
 
 router.get('/contest/list', (req, res) => {
     console.log('contest_list page');
-    res.sendFile(path.join(__dirname, 'contest_list.php?kind=',req.query.kind,'&big=',req.query.big));
+    res.sendFile(path.join(__dirname, '.views/contest_list.php?kind=',req.query.kind,'&big=',req.query.big));
 });
 
 router.get('/contest/view', (req, res) => {
     console.log('contest_view page');
-    res.sendFile(path.join(__dirname, 'contest_view.php?',));
+    res.sendFile(path.join(__dirname, '.views/contest_view.php?',));
 });
 
 
 router.get('/note/send', (req, res) => {
     console.log('note_send page');
-    res.sendFile(path.join(__dirname, 'note_send.php'));
+    res.sendFile(path.join(__dirname, '.views/note_send.php'));
 });
 
 router.get('/note/recv', (req, res) => {
     console.log('note_recv page');
-    res.sendFile(path.join(__dirname, 'note_recv.php'));
+    res.sendFile(path.join(__dirname, '.views/note_recv.php'));
 });
 
 router.get('/mypage', (req, res) => {
     console.log('mypage page');
-    res.sendFile(path.join(__dirname, 'mypage.php'));
+    res.sendFile(path.join(__dirname, '.views/mypage.php'));
 });
+
 module.exports = router; // 모듈로 만드는 부분
