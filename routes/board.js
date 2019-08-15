075c462b4494e3a2932f57148146bb4dc382a45d
kyw017763 <kyw017763@gmail.com>
2019-08-16 05:22:20 +0900

    item_list search, range, page setting

--- routes/board.js
+++ routes/board.js
@@ -23,9 +23,11 @@ router.get('/:kind/list', (req, res) => {
     console.log('It\'s board list page');
 
     let kind = req.params.kind;
-    let item_list;
-    let page_title;
-    
+    let item_list = null;
+    let page_title = null;
+    let page, scale;
+
+    // item_list, page_title
     if (kind==='study') {
         item_list = Study.allItem();
         page_title = '모든 스터디';
@@ -34,11 +36,43 @@ router.get('/:kind/list', (req, res) => {
         page_title = '모든 공모전';
     }
 
+    // range
+    let range = null;
+    if(req.query.range){
+        range = req.query.range;
+        if(range==='num'){
+            item_list.sort((now, next) => now.num - next.num);
+        } else if(range==='author') {
+            item_list.sort((now, next) => now.author - next.author);
+        } else if(range==='topic'){
+            item_list.sort((now, next) => now.topic - next.topic);
+        } else if(range==='title'){
+            item_list.sort((now, next) => now.title - next.title);
+        }
+    }
+
     console.log(item_list);
     console.log(page_title);
 
+    // scale, page
+    if(req.cookies.scale !== undefined) {
+        scale = req.cookies.scale;
+    } else {
+        let s = 10;
+        res.cookie('scale', s);
+        scale = s;
+    }
+    console.log('scale : '+scale);
+
+    if(req.query.page) {
+        page = req.query.page;
+    } else {
+        page = 1;
+    }
+
     let sess = req.session;
     
+    // cookies
     let cid = null;
     let cname = null;
     if(req.cookies.cookie_id !== undefined) {
@@ -50,6 +84,7 @@ router.get('/:kind/list', (req, res) => {
         console.log('cookie_name : '+cname);
     }
 
+    // user_list_num
     let user_list_num;
     Member.findOne({id : cid}).select('list_num').exec(function(err, userNum) {
         user_list_num = userNum.list_num;
@@ -63,21 +98,112 @@ router.get('/:kind/list', (req, res) => {
         list: item_list,
         ptitle: page_title,
         kind: kind,
-        user_list_num: user_list_num
+        user_list_num: user_list_num,
+        page: page,
+        scale: scale
     });
     res.end();
 });
 
+router.get('/:kind/list/search', (req, res) => {
+    console.log('It\'s board list search page');
+
+    let key = req.body.search;
+    
+    let kind = req.params.kind;
+    let item_list = null;
+    let page_title = null;
+    let page, scale; // page => get방식, scale => cookie
+    
+    // item_list, page_title
+    if (kind==='study') {
+        item_list = Study.findItem(key);
+        page_title = '검색된 스터디';
+    } else if (kind==='contest') {
+        item_list = Contest.findItem(key);
+        page_title = '검색된 공모전';
+    }
+
+    console.log(item_list);
+    console.log(page_title);
+
+    // scale, page
+    if(req.cookies.scale !== undefined) {
+        scale = req.cookies.scale;
+    } else {
+        let s = 10;
+        res.cookie('scale', s);
+        scale = s;
+    }
+    console.log('scale : '+scale);
+
+    if(req.query.page) {
+        page = req.query.page;
+    } else {
+        page = 1;
+    }
+
+    let sess = req.session;
+    
+    // cookies
+    let cid = null;
+    let cname = null;
+    if(req.cookies.cookie_id !== undefined) {
+        cid = req.cookies.cookie_id;
+        console.log('cookie_id : '+cid);
+    }
+    if(req.cookies.cookie_id !== undefined) {
+        cname = req.cookies.cookie_name;
+        console.log('cookie_name : '+cname);
+    }
+
+    // user_list_num
+    let user_list_num;
+    Member.findOne({id : cid}).select('list_num').exec(function(err, userNum) {
+        user_list_num = userNum.list_num;
+    });
+    console.log('list_num : '+user_list_num);
+
+    res.setHeader('Content-Type', 'text/html');
+    res.render(path.join(__dirname, '..', 'views', 'item_list.ejs'), {
+        cookie_id: cid,
+        cookie_name: cname,
+        list: item_list,
+        ptitle: page_title,
+        kind: kind,
+        user_list_num: user_list_num,
+        key: key
+    });
+});
+
 // board list subject
 router.get('/:kind/list/:subject', (req, res) => {
-    console.log('It\'s board list subjec page');
+    console.log('It\'s board list subject page');
 
     let kind = req.params.kind;
     let subject = req.params.subject;
 
-    let item_list;
-    let page_title;
+    let item_list = null;
+    let page_title = null;
+    let page, scale; // page => get방식, scale => cookie
+    
+    // cookies
+    if(req.cookies.scale !== undefined) {
+        scale = req.cookies.scale;
+        console.log('scale : '+scale);
+    } else {
+        let s = 10;
+        res.cookie('scale', s);
+        scale = s;
+    }
 
+    if(req.query.page) {
+        page = req.query.page;
+    } else {
+        page = 1;
+    }
+
+    // item_list, page_title
     if (kind==='study') {
         item_list = Study.subjectItem(subject);
         if(subject==='develop') {
@@ -103,14 +229,18 @@ router.get('/:kind/list/:subject', (req, res) => {
     console.log(item_list);
     console.log(page_title);
  
-
     let sess = req.session;
-    res.setHeader('Content-Type', 'text/html');
 
+    // cookies
     let cid = null;
+    let cname = null;
     if(req.cookies.cookie_id !== undefined) {
         cid = req.cookies.cookie_id;
         console.log('cookie_id : '+cid);
+    }
+    if(req.cookies.cookie_id !== undefined) {
+        cname = req.cookies.cookie_name;
+        console.log('cookie_name : '+cname);
     }
 
     let user_list_num;
@@ -127,7 +257,9 @@ router.get('/:kind/list/:subject', (req, res) => {
         ptitle: page_title,
         kind: kind,
         subject: subject,
-        user_list_num: user_list_num
+        user_list_num: user_list_num,
+        page: page,
+        scale: scale
     });
     res.end();
 });
@@ -136,6 +268,32 @@ router.get('/:kind/list/:subject', (req, res) => {
 router.get('/:kind/view', (req, res) => {
     console.log('It\'s board view page');
     let id = req.params.id; // view id
+
+    // cookies
+    let cid = null;
+    let cname = null;
+    if(req.cookies.cookie_id !== undefined) {
+        cid = req.cookies.cookie_id;
+        console.log('cookie_id : '+cid);
+    }
+    if(req.cookies.cookie_id !== undefined) {
+        cname = req.cookies.cookie_name;
+        console.log('cookie_name : '+cname);
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.render(path.join(__dirname, '..', 'views', 'item_view.ejs'), {
+        cookie_id: cid,
+        cookie_name: cname,
+        list: item_list,
+        ptitle: page_title,
+        kind: kind,
+        subject: subject,
+        user_list_num: user_list_num,
+        page: page,
+        scale: scale
+    });
+    res.end();
 });
 
 // wirte, modify, delete, apply, apply_delete
